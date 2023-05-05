69c6b0e38b37910b1837a69734224374ddcf7fa0
edu8686 <85796665+edu8686@users.noreply.github.com>
2023-05-05 14:02:10 -0300

    Actualizacion

--- src/main/java/com/portfolio/jwt/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jwt/Security/Controller/AuthController.java
@@ -27,11 +27,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "http://localhost:4200https://portfolio-ap-dfae9.web.app")
+@CrossOrigin (origins = "https://portfolio-ap-dfae9.web.app", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
