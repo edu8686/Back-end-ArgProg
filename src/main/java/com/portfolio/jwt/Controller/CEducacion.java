85e8a7347dc5c0625ac3e2079d5d5da95cee89b9
edu8686 <85796665+edu8686@users.noreply.github.com>
2023-05-13 09:44:27 -0300

    Actualizacion back

--- src/main/java/com/portfolio/jwt/Controller/CEducacion.java
+++ src/main/java/com/portfolio/jwt/Controller/CEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "https://portfolio-ap-dfae9.firebaseapp.com/", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "https://portfolio-ap-dfae9.web.app/", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
