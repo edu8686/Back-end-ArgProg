7a5620be99981119cdce7e0efa855f84d537e33c
edu8686 <85796665+edu8686@users.noreply.github.com>
2023-05-13 13:27:18 -0300

    Modificacion clase principal

--- src/main/java/com/portfolio/jwt/JwtApplication.java
+++ src/main/java/com/portfolio/jwt/JwtApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "https://portfolio-ap-dfae9.web.app/")
+@CrossOrigin(origins = "https://portfolio-ap-dfae9.web.app")
 @SpringBootApplication
 public class JwtApplication {
 
